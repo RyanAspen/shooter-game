1bb2db48904fa41f530527e89fe1fa46a91184ba
Ryan A <ryanaspen27@gmail.com>
2022-07-26 15:10:42 -0400

    Notes on attributes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+TODO: 
+- Add standard entity attributes that can be added to entities
+   * Rigid_body -> cannot overlap with any other entity with Rigid Body
+   * Constrained_to_screen -> bounces off of edges of the screen
+   * Destroyed_off_screen -> destroys itself after leaving the screen
+   * Gravity -> experiences gravity downwards
+"""
+
 from basic_scene import BasicScene
 from projectile_scene import ProjectileScene
 from screen import Screen
