56496b7aae318da889f1696384d830578267428e
Ryan A <ryanaspen27@gmail.com>
2022-07-30 19:38:14 -0400

    Removed unnecessary checks in collision manager

--- entity_collision_manager.py
+++ entity_collision_manager.py
@@ -30,28 +30,29 @@ class EntityCollisionManager:
         new_collisions = dict()
         for entity1 in self.entities:
             for entity2 in self.entities:
-                if are_different(entity1, entity2):
-                    if are_colliding(entity1, entity2):
-                        collision_tuple_1 = (
-                            entity2.name,
-                            entity2.current_point,
-                        )
-                        collision_tuple_2 = (
-                            entity1.name,
-                            entity1.current_point,
-                        )
-                        if entity1 not in new_collisions:
-                            new_collisions[entity1.id] = [collision_tuple_1]
-                        else:
-                            new_collisions[entity1.id].append(collision_tuple_1)
-                        if entity2 not in new_collisions:
-                            new_collisions[entity2.id] = [collision_tuple_2]
-                        else:
-                            new_collisions[entity2.id].append(collision_tuple_2)
+                if are_colliding(entity1, entity2):
+                    collision_tuple_1 = (
+                        entity2.name,
+                        entity2.current_point,
+                    )
+                    collision_tuple_2 = (
+                        entity1.name,
+                        entity1.current_point,
+                    )
+                    if entity1 not in new_collisions:
+                        new_collisions[entity1.id] = [collision_tuple_1]
+                    else:
+                        new_collisions[entity1.id].append(collision_tuple_1)
+                    if entity2 not in new_collisions:
+                        new_collisions[entity2.id] = [collision_tuple_2]
+                    else:
+                        new_collisions[entity2.id].append(collision_tuple_2)
         return new_collisions
 
 
 def are_colliding(entity_1: PixelEntity, entity_2: PixelEntity) -> bool:
+    if entity_1.id == entity_2.id:
+        return False
     for rect1 in entity_1.current_frame.hitboxes:
         for rect2 in entity_2.current_frame.hitboxes:
             if rect1.colliderect(rect2):
